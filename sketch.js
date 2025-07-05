71f3f12b82dbca7d03c87e2c612864b9e54313a1
Karim Oyarzabal <karim_oyarzabal@Karims-MacBook-Pro-4.local>
2025-07-05 17:31:35 +0200

    first commit de mon sketch Rencontre_1

---
+++ sketch.js
@@ -0,0 +1,24 @@
+let xoff = 0.0;
+let moff = 2.0;
+
+function setup() {
+  createCanvas(640, 320);
+  colorMode(HSB, 360, 100, 100, 255);
+  fill(0, 80, 50, 200);
+}
+
+function draw() {
+  background(30, 70, 70, 255);
+
+  stroke(60, 100, 100, 170);
+  strokeWeight(40);
+  xoff += 0.01;
+  let n = noise(xoff) * width;
+  line(n, 0, n, height);
+
+  stroke(40, 100, 100, 190);
+  strokeWeight(25);
+  moff += 0.01;
+  let m = noise(moff) * width;
+  line(m, 0, m, height);
+}
